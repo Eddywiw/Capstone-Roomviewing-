5845d9277a75b27d4d4e5fc8c9f89a8606634c7f
Edchristian Besiyos <ebesiyos76@gmail.com>
2023-10-11 09:11:50 +0800

    color changes

--- capstone/src/pages/AdminHome.jsx
+++ capstone/src/pages/AdminHome.jsx
@@ -3,24 +3,29 @@ import ScheduleTBL from '../components/scheduleTBL';
 import './AdminHome.css'
 function AdminHome() {
   return (
-    <div>
-      <div className='lbl_dashboard'>
-        <p>My Dashboard</p>
-      </div>
-      <div className='tota-con'>
-        <div className='totalstudents'>
-            <p>0</p>
-            <p>Total Students</p>
+    <div className='mainadhome-con'>
+      <div className='bgcolor'>
+        <div className='lbl_dashboard'>
+          <p>My Dashboard</p>
         </div>
+        <div className='tota-con'>
+          <div className='totalstudents'>
+              <p>0</p>
+              <p>Total Students</p>
+          </div>
 
-        <div className='totalprof'>
-            <p>0</p>
-            <p>Total Professors</p>
+          <div className='totalprof'>
+              <p>0</p>
+              <p>Total Professors</p>
+          </div>
         </div>
-        <div className="schedule-con">
-          <ScheduleTBL/>
-        </div>
+        
+          <div className="schedule-con">
+            <ScheduleTBL/>
+          </div>
       </div>
+      
+      
     </div>
   )
 }
