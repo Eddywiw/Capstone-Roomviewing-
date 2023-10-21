f09f9f3e5077aa19e441a1f507c500b838aeb160
Edchristian <ebesiyos76@gmail.com>
2023-10-21 23:04:19 +0800

    excel feature

--- capstone/src/components/Users.jsx
+++ capstone/src/components/Users.jsx
@@ -8,10 +8,13 @@ import { doc, deleteDoc } from "firebase/firestore";
 import './Users.css'
 import NotificationForm from './Notification';
 import {db} from '../config/firestore'
-
+import { addDoc } from 'firebase/firestore';
 import Calendar from './calendar';
 import StudentTable from './StudentTable';
+import * as XLSX from 'xlsx';
+
 function Users() {
+  
     //bago to
     const [selectedSection, setSelectedSection] = useState('bsit'); // Initialize with 'bsit' 
   
@@ -20,6 +23,49 @@ function Users() {
       { value: 'BSBA', label: 'BSBA' },
       { value: 'HRS', label: 'HRS' },
     ];
+    const handleExcelUpload = (event) => {
+      const file = event.target.files[0];
+    
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const data = new Uint8Array(e.target.result);
+          const workbook = XLSX.read(data, { type: 'array' });
+          const sheetName = workbook.SheetNames[0]; // Assuming data is in the first sheet
+    
+          const sheet = workbook.Sheets[sheetName];
+          const jsonData = XLSX.utils.sheet_to_json(sheet);
+    
+          // Now you have jsonData with the Excel data, you can add it to Firestore and update your state.
+          addDataToFirestore(jsonData);
+        };
+    
+        reader.readAsArrayBuffer(file);
+      }
+    };
+    
+    const addDataToFirestore = (jsonData) => {
+      jsonData.forEach(async (row) => {
+        try {
+          // Log the data before adding it to Firestore to identify any issues
+          console.log("Adding data to Firestore:", row);
+    
+          const docRef = await addDoc(collection(db, selectedSection), {
+            Name: row.Name,
+            Studentno: row.Studentno,
+            Section: row.Section,
+            Email: row.Email,
+            Password: row.Password,
+          });
+    
+          // Update the state to reflect the newly added user
+          setUsers((prevUsers) => [...prevUsers, { id: docRef.id, ...row }]);
+        } catch (error) {
+          console.error('Error adding document: ', error);
+        }
+      });
+    };
+        
   
     const handleDropdownChange = (event) => {
       setSelectedOption(event.target.value);
@@ -139,6 +185,10 @@ function Users() {
             }
           </tbody>
         </table>
+        <input type="file" accept=".xls,.xlsx" onChange={handleExcelUpload} />
+
+
+
       </div>
 
       
